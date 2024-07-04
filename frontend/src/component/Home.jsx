168d2377f828a689c0d274c837d314bdb4fd154f
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-07-04 11:52:36 +0530

    Added recent Music Section

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
+import img from './images.jpg'
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { Layout, Menu, ConfigProvider } from 'antd';
-
+import { SearchOutlined,DownOutlined } from '@ant-design/icons';
+import { Button, Dropdown,Avatar  , Space } from 'antd';
+import Card from './ui/Card';
 const { Header, Content, Footer, Sider } = Layout;
-
 const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
   (icon, index) => ({
     key: String(index + 1),
@@ -34,18 +36,97 @@ const App = () => {
           }}
           style={{ backgroundColor: 'rgba(0,0,0,0.1)' }}
         >
-          <div className="demo-logo-vertical" style={{ color: 'black', textAlign: 'center', fontSize: "20px", padding: "10px" }}>
+          <div className="demo-logo-vertical" style={{ color: 'black', textAlign: 'center', fontFamily:"sans-serif",fontSize: "25px", padding: "20px 0" }}>
             Rhythmix
           </div>
           <Menu style={{ backgroundColor: 'transparent' }} defaultSelectedKeys={['4']} items={items} />
         </Sider>
 
         <Layout style={{ backgroundColor: 'transparent' }}>
-          <Header style={{ padding: 0, background: 'transparent' }} />
-          <Content style={{ margin: '24px 16px 0', backgroundColor: 'transparent' }}>
-            <div style={{ padding: 24, minHeight: 360, borderRadius: '8px' }}>
-              content
-            </div>
+
+          <Header style={{ padding: 0, background: 'transparent',display:'flex',alignItems:'center',justifyContent:'space-between' }} >
+
+          <Space direction="vertical" size="middle">
+          <Space.Compact size="large" style={{ margin: '20px 16px', background: 'transparent' }}>
+      <div className="div" style={{ border: '0.1px solid grey', borderRadius: "7px", display: 'flex', alignItems: 'center' }}>
+        <SearchOutlined style={{ padding: "0 10px", fontSize: "20px" }} />
+        <input 
+          type='text' 
+          style={{ 
+            fontSize: "20px", 
+            width: '20em', 
+            height: '40px', 
+            padding: "5px 0", 
+            backgroundColor: 'transparent', 
+            border: 'none', 
+            lineHeight: '20px' 
+          }} 
+          onFocus={(e) => { e.target.style.outline = "none"; }} 
+          placeholder="Search track, album, artists..."
+        />
+
+      </div>
+
+    </Space.Compact>
+        </Space>
+  
+        <Space direction="vertical">
+    <Space wrap style={{marginRight:"90px"}} >
+    <Dropdown
+        menu={{
+          items,
+        }}
+        placement="bottom"
+        arrow={{
+          pointAtCenter: true,
+        }}
+        style={{background:'transparent'}}
+      >
+        <Button className='glow-on-hover' > 
+        <Avatar
+    size={{
+      xs: 24,
+      sm: 28,
+      md: 30,
+      lg: 35,
+      xl: 35,
+      xxl: 35,
+    }}
+    icon={<img src={img}></img>}
+  />
+ Chetax
+  <DownOutlined />
+
+        </Button>
+      </Dropdown>
+      </Space>
+  </Space>
+
+
+          </Header>
+          <Content style={{ margin: '15px 10px', backgroundColor: 'transparent' }}>
+          
+          <div style={{ padding: 24, minHeight: 36, borderRadius: '8px' ,color:"white",display:"flex",alignItems:'center',justifyContent:'space-between'}}>
+          <p style={{fontSize:"20px"}}>  Recenttly Plays</p>
+          <p style={{fontSize:"15px",marginRight:"20px"}}> See all</p>
+          </div>
+
+        {/* map on the recent plaed song which are stored in DB */}
+        
+<div style={{display:'flex',overflowX: 'scroll',  scrollbarWidth: 'none',}} >
+<Card/>
+<Card/>
+<Card/>
+<Card/>
+<Card/>
+<Card/>
+<Card/>
+<Card/>
+  </div> 
+
+
+
+
           </Content>
           <Footer style={{ textAlign: 'center', background: 'transparent' }}>
           Rhythmix Design ©{new Date().getFullYear()} Created by Chetan Padhen
---
+++ frontend/src/component/ui/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from 'antd';
+const { Meta } = Card;
+const App = () => (
+ <>
+ <div style={{margin:"0 10px",cursor:'pointer'}}> 
+ <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:"170px",}} />
+ <div style={{textAlign:'center',margin:"10px 0"}}>
+    <p style={{fontSize:'17px'}}>Coldplay</p>
+    <p>Are Of Music</p>
+ </div>
+ </div>
+ </>
+);
+export default App;
